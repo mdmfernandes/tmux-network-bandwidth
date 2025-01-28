8d7d6cc11a41d506ec214e67c31ba8b497abdf81
Miguel Fernandes <me@mdmfernandes.com>
2025-01-28 00:18:39 +0000

    refactor: all

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -3,40 +3,40 @@
 PATH="/usr/local/bin:$PATH:/usr/sbin"
 
 get_tmux_option() {
-  local option_name="$1"
-  local default_value="$2"
-  local option_value="$(tmux show-option -gqv $option_name)"
+    local option_name="$1"
+    local default_value="$2"
+    local option_value="$(tmux show-option -gqv $option_name)"
 
-  if [ -z "$option_value" ]; then
-    echo -n "$default_value"
-  else
-    echo -n "$option_value"
-  fi
+    if [ -z "$option_value" ]; then
+        echo -n "$default_value"
+    else
+        echo -n "$option_value"
+    fi
 }
 
 set_tmux_option() {
-  local option_name="$1"
-  local option_value="$2"
-  $(tmux set-option -gq $option_name "$option_value")
+    local option_name="$1"
+    local option_value="$2"
+    $(tmux set-option -gq $option_name "$option_value")
 }
 
 os_type() {
-  local os_name="unknown"
+    local os_name="unknown"
 
-  case $(uname | tr '[:upper:]' '[:lower:]') in
+    case $(uname | tr '[:upper:]' '[:lower:]') in
     linux*)
-      os_name="linux"
-      ;;
+        os_name="linux"
+        ;;
     darwin*)
-      os_name="osx"
-      ;;
+        os_name="osx"
+        ;;
     msys*)
-      os_name="windows"
-      ;;
+        os_name="windows"
+        ;;
     freebsd*)
-      os_name="freebsd"
-      ;;
-  esac
+        os_name="freebsd"
+        ;;
+    esac
 
-  echo -n $os_name
+    echo -n $os_name
 }
--- scripts/network-bandwidth.sh
+++ scripts/network-bandwidth.sh
@@ -3,9 +3,8 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/helpers.sh"
 
-
 get_bandwidth_for_osx() {
-  netstat -ibn | awk 'FNR > 1 {
+    netstat -ibn | awk 'FNR > 1 {
     interfaces[$1 ":bytesReceived"] = $(NF-4);
     interfaces[$1 ":bytesSent"]     = $(NF-1);
   } END {
@@ -21,56 +20,56 @@ get_bandwidth_for_osx() {
 }
 
 get_bandwidth_for_linux() {
-    /bin/ip -j -s link show |\
+    /bin/ip -j -s link show |
         jq -r '[.[].stats64] | "\(map(.rx.bytes) | add) \(map(.tx.bytes) | add)"'
 }
 
 get_bandwidth() {
-  local os="$1"
+    local os="$1"
 
-  case $os in
+    case $os in
     osx)
-      echo -n $(get_bandwidth_for_osx)
-      return 0
-      ;;
+        echo -n $(get_bandwidth_for_osx)
+        return 0
+        ;;
     linux)
-      echo -n $(get_bandwidth_for_linux)
-      return 0
-      ;;
+        echo -n $(get_bandwidth_for_linux)
+        return 0
+        ;;
     *)
-      echo -n "0 0"
-      return 1
-      ;;
-  esac
+        echo -n "0 0"
+        return 1
+        ;;
+    esac
 }
 
 format_speed() {
-  local padding=$(get_tmux_option "@tmux-network-bandwidth-padding" 5)
-  local suffix=$(get_tmux_option "@tmux-network-bandwidth-suffix" "B/s")
-  local format=$(get_tmux_option "@tmux-network-bandwidth-format" "%f")
+    local padding=$(get_tmux_option "@tmux-network-bandwidth-padding" 5)
+    local suffix=$(get_tmux_option "@tmux-network-bandwidth-suffix" "B/s")
+    local format=$(get_tmux_option "@tmux-network-bandwidth-format" "%f")
 
-  numfmt --to=iec --suffix $suffix --format $format --padding $padding $1
+    numfmt --to=iec --suffix $suffix --format $format --padding $padding $1
 }
 
 main() {
-  local sleep_time=$(get_tmux_option "status-interval")
-  local old_value=$(get_tmux_option "@network-bandwidth-previous-value")
+    local sleep_time=$(get_tmux_option "status-interval")
+    local old_value=$(get_tmux_option "@network-bandwidth-previous-value")
 
-  if [ -z "$old_value" ]; then
-    $(set_tmux_option "@network-bandwidth-previous-value" "-")
-    echo -n "Please wait..."
-    return 0
-  else
-    local os=$(os_type)
-    local first_measure=( $(get_bandwidth $os) )
-    sleep $sleep_time
-    local second_measure=( $(get_bandwidth $os) )
-    local download_speed=$(((${second_measure[0]} - ${first_measure[0]}) / $sleep_time))
-    local upload_speed=$(((${second_measure[1]} - ${first_measure[1]}) / $sleep_time))
-    $(set_tmux_option "@network-bandwidth-previous-value" "↓$(format_speed $download_speed) • ↑$(format_speed $upload_speed)")
-  fi
+    if [ -z "$old_value" ]; then
+        $(set_tmux_option "@network-bandwidth-previous-value" "-")
+        echo -n "Please wait..."
+        return 0
+    else
+        local os=$(os_type)
+        local first_measure=($(get_bandwidth $os))
+        sleep $sleep_time
+        local second_measure=($(get_bandwidth $os))
+        local download_speed=$(((${second_measure[0]} - ${first_measure[0]}) / $sleep_time))
+        local upload_speed=$(((${second_measure[1]} - ${first_measure[1]}) / $sleep_time))
+        $(set_tmux_option "@network-bandwidth-previous-value" "↓$(format_speed $download_speed) ↑$(format_speed $upload_speed)")
+    fi
 
-  echo -n "$(get_tmux_option "@network-bandwidth-previous-value")"
+    echo -n "$(get_tmux_option "@network-bandwidth-previous-value")"
 }
 
 main
